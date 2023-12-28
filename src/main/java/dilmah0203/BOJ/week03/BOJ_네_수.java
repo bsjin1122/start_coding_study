9d69aa55af20838c9f7085359b7105bf998a669a
dilmah0203 <dilmah0203@naver,com>
2023-12-28 23:26:00 +0900

    2023.12.28

---
+++ src/main/java/dilmah0203/BOJ/week03/BOJ_네_수.java
@@ -0,0 +1,26 @@
+package src.main.java.dilmah0203.BOJ.week03;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_네_수 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        String array[] = new String[4];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = st.nextToken();
+        }
+
+        String a = array[0] + array[1];
+        String b = array[2] + array[3];
+
+        Long sum = Long.parseLong(a) + Long.parseLong(b);
+
+        System.out.println(sum);
+    }
+}
