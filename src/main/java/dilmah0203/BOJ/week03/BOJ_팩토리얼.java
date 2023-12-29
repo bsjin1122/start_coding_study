53c9a22efe58379e73b96956ae4650a4a1819f95
dilmah0203 <dilmah0203@naver,com>
2023-12-29 12:24:53 +0900

    2023.12.29

---
+++ src/main/java/dilmah0203/BOJ/week03/BOJ_팩토리얼.java
@@ -0,0 +1,20 @@
+package src.main.java.dilmah0203.BOJ.week03;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_팩토리얼 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int result = 1;
+        while(n > 0) {
+            result *= n;
+            n--;
+        }
+        System.out.println(result);
+    }
+}
