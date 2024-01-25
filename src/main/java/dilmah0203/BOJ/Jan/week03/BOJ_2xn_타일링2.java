3d86fa56962f9162698f30cb094e04410b31ecb9
dilmah0203 <dilmah0203@naver.com>
2024-01-25 22:11:20 +0900

    2024.1.25

---
+++ src/main/java/dilmah0203/BOJ/Jan/week03/BOJ_2xn_타일링2.java
@@ -0,0 +1,23 @@
+package src.main.java.dilmah0203.BOJ.Jan.week03;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_2xn_타일링2 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine()); //2 x n 직사각형
+
+        int[] dp = new int[1001];
+        dp[1] = 1;
+        dp[2] = 3;
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i - 1] + 2 * dp[i - 2]) % 10007;
+        }
+        System.out.println(dp[n]);
+    }
+}
